ec913446b9a3ee47f63502b00274788bb5cefe47
Яков Савельев <jakobs@Akovs-MacBook-Pro.local>
2025-12-09 10:27:29 +0100

    apply-flowbite-react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+import flowbitePlugin from "flowbite/plugin";
 
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        white: "#FFFFFF",
-        "button-black": "#252829",
-        "dark-black": "#000000",
-        "button-soft-black": "#292A2D",
-        "background-black": "#1F1F1F",
-        "side-menu-black": "#232425",
-        "light-blue": "#00C6FF",
-        "dark-blue": "#0072C5",
-        "soft-blue": "#4688F1",
-        "title-gray": "#9C9C9C",
-        "dark-gray": "#717170",
-        "opacity-gray": "#35353580",
-        "background-gray": "#F3F3F3",
-        "disabled-gray": "#E2E2E2",
-        "input-error": "#FF6058",
-        "input-success": "text-green-500",
-        "button-wite": "#FAFAFA",
-        "input-black": "#2f303270",
-        gold: "#FFD700",
-      },
-    },
-  },
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.js",
+  ],
+  theme: { extend: {} },
+  plugins: [flowbitePlugin],
 };
